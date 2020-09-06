f52deb9f5ed73811dcab262ff3567d96e26eaa31
John Wright <jrite@protonmail.com>
2020-09-06 21:36:54 +1000

    test: add react-testing-library

---
+++ src/components/__tests__/App.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from '../App'
+
+jest.mock('../Hello', () => () => '{Hello}')
+
+test('App', () => {
+  const component = render(<App />)
+  expect(component.container).toMatchInlineSnapshot(`
+    <div>
+      <div
+        class="MuiContainer-root MuiContainer-maxWidthLg"
+      >
+        {Hello}
+      </div>
+    </div>
+  `)
+})
