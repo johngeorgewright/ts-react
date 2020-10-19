52886106a542b7512f15344b2befd200bcc528ea
John Wright <jrite@protonmail.com>
2020-10-20 00:01:25 +1100

    feat: assert-never

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,28 +1,39 @@
 import React, { useCallback } from 'react'
 import { Typography, Button } from '@material-ui/core'
+import assertNever from 'assert-never'
 import useCycle from '../hooks/useCycle'
 
-const states = ['Morning', 'Afternoon', 'Evening']
+enum State {
+  morning = 'Morning',
+  afternoon = 'Afternoon',
+  evening = 'Evening',
+}
 
 export default function Hello() {
-  const [state, nextState] = useCycle(states)
+  const [state, nextState] = useCycle(Object.values(State))
   const updateState = useCallback(nextState, [state])
-
-  const color =
-    state === 'Morning'
-      ? 'default'
-      : state === 'Afternoon'
-      ? 'primary'
-      : 'secondary'
 
   return (
     <>
       <Typography variant="h3" component="h1" gutterBottom>
         @johngeorgewright/ts-react
       </Typography>
-      <Button variant="contained" color={color} onClick={updateState}>
+      <Button variant="contained" color={color(state)} onClick={updateState}>
         {state} World
       </Button>
     </>
   )
+}
+
+function color(state: State) {
+  switch (state) {
+    case State.morning:
+      return 'default'
+    case State.afternoon:
+      return 'primary'
+    case State.evening:
+      return 'secondary'
+    default:
+      return assertNever(state)
+  }
 }
