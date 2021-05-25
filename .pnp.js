ae4d40b18f75a9cf5367509d8c8047891e0648da
John Wright <jrite@protonmail.com>
2021-05-25 17:44:33 +0100

    feat: use tslib

--- .pnp.js
+++ .pnp.js
@@ -145,6 +145,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
                     'ts-node',
                     'virtual:7ba3a1726cf6a1230dd803b30b5e43e1f63943b3638817a30ba4245a3992b9e64acaeb10a7a650968a24664bbb29f46daee46a9b1f72164b0616dafe5552cca8#npm:9.1.1',
                   ],
+                  ['tslib', 'npm:2.2.0'],
                   [
                     'typescript',
                     'patch:typescript@npm%3A4.2.4#builtin<compat/typescript>::version=4.2.4&hash=a45b0e',
@@ -4977,6 +4978,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
                     'ts-node',
                     'virtual:7ba3a1726cf6a1230dd803b30b5e43e1f63943b3638817a30ba4245a3992b9e64acaeb10a7a650968a24664bbb29f46daee46a9b1f72164b0616dafe5552cca8#npm:9.1.1',
                   ],
+                  ['tslib', 'npm:2.2.0'],
                   [
                     'typescript',
                     'patch:typescript@npm%3A4.2.4#builtin<compat/typescript>::version=4.2.4&hash=a45b0e',
