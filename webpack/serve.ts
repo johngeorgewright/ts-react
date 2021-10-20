d59e4e0e1a40ea842bd01ec7a3731269959e2b0f
John Wright <jrite@protonmail.com>
2021-10-20 19:16:11 +0100

    fix: types

--- webpack/serve.ts
+++ webpack/serve.ts
@@ -1,4 +1,9 @@
-import { Configuration } from 'webpack'
+import { Configuration as WebpackConfiguration } from 'webpack'
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration
+}
 
 const config: Configuration = {
   devServer: {
