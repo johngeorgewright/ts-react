7f93d74f7179af04d990b7af5c1640fa42aaf623
John Wright <jrite@protonmail.com>
2022-01-02 21:53:31 +0000

    refactor: use webpack-dev-server config
    There's no longer any need for custom config.

--- webpack/serve.ts
+++ webpack/serve.ts
@@ -1,10 +1,9 @@
 import { Configuration } from 'webpack'
+import 'webpack-dev-server'
 
 const config: Configuration = {
   devServer: {
     hot: true,
-    // @ts-ignore This is new in webpack-dev-server@v4
-    // but it is yet to be included in @types/webpack-dev-server
     static: {
       directory: './dist',
     },
